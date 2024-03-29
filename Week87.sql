6616bcb952eb0262c55aad8f28b396d08b74b2b1
Darko Monzio Compagnoni <144227582+darko-nimbus@users.noreply.github.com>
2024-03-29 15:34:34 +0100

    Create Week87.sql
    Solution to Frosty Friday challenge week 87

---
+++ Week87.sql
@@ -0,0 +1,15 @@
+--START UP CODE--
+CREATE OR REPLACE TABLE WEEK_87 AS
+SELECT 
+  'Happy Easter' AS greeting,
+  ARRAY_CONSTRUCT('DE', 'FR', 'IT', 'ES', 'PL', 'RO', 'JA', 'KO', 'PT') AS language_codes;
+
+--CHECK--
+SELECT * FROM week_87;
+
+--SOLUTION--
+SELECT 
+  value::VARCHAR AS language_code, 
+  SNOWFLAKE.CORTEX.TRANSLATE(greeting, 'en', value) AS translated_greeting
+FROM WEEK_87,
+LATERAL FLATTEN(input => language_codes);
