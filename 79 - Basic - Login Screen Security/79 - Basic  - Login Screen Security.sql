63912592e778fc5d428ed7606d8796021dde47c3
Darko Monzio Compagnoni <144227582+darko-nimbus@users.noreply.github.com>
2024-04-05 11:10:48 +0200

    Create 79 - Basic  - Login Screen Security.sql

---
+++ 79 - Basic - Login Screen Security/79 - Basic  - Login Screen Security.sql
@@ -0,0 +1,14 @@
+-- Creating user --
+CREATE OR REPLACE USER user1 
+PASSWORD = 'abc123'
+;
+
+-- Creating the authentication policy --
+CREATE AUTHENTICATION POLICY access_only_via_snowflake_ui_with_username_password
+  CLIENT_TYPES = ('SNOWFLAKE_UI')
+  AUTHENTICATION_METHODS = ('PASSWORD')
+  COMMENT = 'user can access only via the Snowflake UI using username
+  and password';
+
+-- Assigning the authentication policy to the user --
+ALTER USER user1 SET AUTHENTICATION POLICY access_only_via_snowflake_ui_with_username_password;
