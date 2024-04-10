5b8234179fe804847c27d8ba902698f2a210fb99
Darko Monzio Compagnoni <144227582+darko-nimbus@users.noreply.github.com>
2024-04-10 09:40:12 +0200

    Update 74 - Intermediate - Conditional Expression Function.sql

--- 74 - Intermediate - Conditional Expression Function/74 - Intermediate - Conditional Expression Function.sql
+++ 74 - Intermediate - Conditional Expression Function/74 - Intermediate - Conditional Expression Function.sql
@@ -1,36 +1,40 @@
+-- Set the current database context to 'FROSTYFRIDAY'
 USE DATABASE FROSTYFRIDAY;
 
--- Creating table -- 
+------------------
+-- STARTUP CODE --
+------------------
 
-create or replace table dates (
-    birthdate varchar
-)
-as
-select * from values
-    ('22/09/1968')
-  , ('1968-09-22')
-  , ('1968-09-22')
-  , ('1968-09-22')
-  , ('1968-09-22')
-  , ('1968-09-22')
-  , ('1968-09-22')
-  , ('1968-09-22')
-  , ('22/09/1968')
-  , ('27/08/1985')
-  , ('30/07/1967')
-  , ('1985-08-27')
-;
+-- Create or replace the 'dates' table with sample dates in various formats
+CREATE OR REPLACE TABLE dates (
+    birthdate VARCHAR
+) AS
+SELECT * FROM VALUES
+    ('22/09/1968'),
+    ('1968-09-22'),
+    ('1968-09-22'),
+    ('1968-09-22'),
+    ('1968-09-22'),
+    ('1968-09-22'),
+    ('1968-09-22'),
+    ('1968-09-22'),
+    ('22/09/1968'),
+    ('27/08/1985'),
+    ('30/07/1967'),
+    ('1985-08-27');
 
--- Solution -- 
+--------------
+-- SOLUTION --
+--------------
 
-create or replace view dates_formatted
-as
-select
+-- Create or replace a view 'dates_formatted' to display birthdates in a consistent 'DATE' format
+CREATE OR REPLACE VIEW dates_formatted AS
+SELECT
     COALESCE(
-        TRY_TO_DATE(BIRTHDATE, 'YYYY-MM-DD')
-      , TRY_TO_DATE(BIRTHDATE, 'DD/MM/YYYY')
-    ) as BIRTHDATE
-from dates;
+        TRY_TO_DATE(birthdate, 'YYYY-MM-DD'),
+        TRY_TO_DATE(birthdate, 'DD/MM/YYYY')
+    ) AS birthdate
+FROM dates;
 
--- check -- 
-select * from dates_formatted;
+-- Query to check the content of 'dates_formatted'
+SELECT * FROM dates_formatted;
