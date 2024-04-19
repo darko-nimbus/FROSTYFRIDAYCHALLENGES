58818ff8d1a0352ff376c72d6e9860f5772b412f
Darko Monzio Compagnoni <144227582+darko-nimbus@users.noreply.github.com>
2024-04-19 14:57:07 +0200

    Create 62 - Intermediate - Native Apps.py

---
+++ 62 - Intermediate - Native Apps/62 - Intermediate - Native Apps.py
@@ -0,0 +1,7 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+
+# Write directly to the app
+st.title(":snowflake: Hello World! :snowflake:")
+
